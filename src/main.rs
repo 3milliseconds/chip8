d32585715dd2bf4940a43ba005144b24a2f9da23
3ms <258579313+3milliseconds@users.noreply.github.com>
2026-03-28 18:39:28 +0530

    added minifb

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,141 @@
+#![deny(clippy::all)]
+#![forbid(unsafe_code)]
+
 use log::error;
 use log::info;
+use minifb::Key;
+use minifb::{Window, WindowOptions};
 use rand::{RngExt, SeedableRng, rngs::SmallRng};
+
+const WIDTH: usize = 64;
+const HEIGHT: usize = 32;
+
 fn main() {
     env_logger::init();
-    let input = std::fs::read("input/IBM Logo.ch8").unwrap();
-    let mut cpu = CpuState::new();
-    cpu.memory[0x200..(0x200 + input.len())].copy_from_slice(&input);
-    start_cpu(&mut cpu);
+    let mut app = App::init();
+    app.start();
 }
 
-fn start_cpu(state: &mut CpuState) {
-    let mut count = 0;
-    while count < 50 {
-        let pc = state.pc as usize;
+struct CpuState {
+    memory: [u8; 4096],
+    v: [u8; 16],
+    i: u16,
+    dt: u8,
+    st: u16,
+    pc: u16,
+    sp: u8,
+    rng: SmallRng,
+}
+
+impl CpuState {
+    fn new() -> CpuState {
+        return CpuState {
+            memory: [0; 4096],
+            v: [0; 16],
+            i: 0,
+            dt: 0,
+            st: 0,
+            pc: 0x200,
+            sp: 0,
+            rng: SmallRng::seed_from_u64(12345),
+        };
+    }
+}
+
+struct App {
+    chip8: Chip8,
+    window: Window,
+    buffer: Vec<u32>,
+}
+
+impl App {
+    fn init() -> App {
+        let input: Vec<u8> = std::fs::read("input/IBM Logo.ch8").unwrap();
+
+        let buffer: Vec<u32> = vec![0; WIDTH * HEIGHT * 100];
+
+        let mut window: Window = Window::new(
+            "Test - ESC to exit",
+            WIDTH * 10,
+            HEIGHT * 10,
+            WindowOptions::default(),
+        )
+        .unwrap_or_else(|e| {
+            panic!("{}", e);
+        });
+
+        window.set_target_fps(60);
+
+        return App {
+            chip8: Chip8::init(input),
+            window: window,
+            buffer: buffer,
+        };
+    }
+
+    fn start(&mut self) {
+        while self.window.is_open() && !self.window.is_key_down(Key::Escape) {
+            let mut count = 0;
+            while count < 10 {
+                self.chip8.step();
+                count += 1;
+            }
+            for i in 0..WIDTH {
+                for j in 0..HEIGHT {
+                    let bit = self.chip8.display[i];
+                    for k in 0..10 {
+                        for l in 0..10 {
+                            self.buffer[(i * 10 + k) + (j * 10 + l) * WIDTH * 10] = bit as u32;
+                        }
+                    }
+                }
+            }
+            self.window
+                .update_with_buffer(&self.buffer, WIDTH, HEIGHT)
+                .unwrap();
+        }
+    }
+}
+
+struct Chip8 {
+    cpu_state: CpuState,
+    display: [u8; 64 * 32],
+}
+
+impl Chip8 {
+    fn init(input: Vec<u8>) -> Chip8 {
+        let mut cpu = CpuState::new();
+        cpu.memory[0x200..(0x200 + input.len())].copy_from_slice(&input);
+        let sprites: [u8; 80] = [
+            0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+            0x20, 0x60, 0x20, 0x20, 0x70, // 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+            0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+            0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+            0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+            0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+            0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+        ];
+        cpu.memory[0..80].copy_from_slice(&sprites);
+        return Chip8 {
+            cpu_state: cpu,
+            display: [0; 64 * 32],
+        };
+    }
+
+    fn step(&mut self) {
+        let state: &mut CpuState = &mut self.cpu_state;
+        let diplay: &mut [u8; _] = &mut self.display;
+
+        let pc: usize = state.pc as usize;
         let opcode: u16 = ((state.memory[pc] as u16) << 8) | (state.memory[pc + 1] as u16);
         match opcode {
             0x00E0 => {
@@ -26,7 +149,7 @@ fn start_cpu(state: &mut CpuState) {
                 let nnn = opcode & 0x0FFF;
                 info!("{:#06x} Sets the program counter to {nnn}", opcode);
                 state.pc = opcode & 0x0FFF;
-                continue;
+                return;
             }
             _ if (opcode & 0xF000) == 0x2000 => {
                 info!("Call subroutine at nnn");
@@ -120,7 +243,7 @@ fn start_cpu(state: &mut CpuState) {
             }
             _ if (opcode & 0xF000) == 0xA000 => {
                 let nnn = opcode.get_nnn();
-                info!("{:#06x} Set I = {nnn}", opcode);
+                info!("{:#06x} Set I = {:#06x}", opcode, nnn);
                 state.i = nnn;
             }
             _ if (opcode & 0xF000) == 0xB000 => {
@@ -132,10 +255,30 @@ fn start_cpu(state: &mut CpuState) {
                 let (x, kk) = opcode.get_xkk();
                 state.v[x] = state.rng.random::<u8>() & kk;
             }
-            _ if (opcode & 0xF000) == 0xD000 => info!(
-                "{:#06x} Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision",
-                opcode
-            ),
+            _ if (opcode & 0xF000) == 0xD000 => {
+                let (x, y) = opcode.get_xy();
+                let (vx, vy) = (state.v[x] as usize, state.v[y] as usize);
+                let n: u16 = opcode & 0xF;
+                info!(
+                    "{:#06x} Display {n}-byte sprite starting at memory location I({:06x}) at (Vx, Vy), set VF = collision",
+                    opcode, state.i
+                );
+                let i: usize = state.i as usize;
+                let bytes = &state.memory[i..i + n as usize];
+                state.v[0xf] = 0;
+                for (index, byte) in bytes.iter().enumerate() {
+                    let byte = *byte;
+                    for bit_pos in 0..=7 {
+                        let bit = (byte >> (7 - bit_pos)) & 1;
+                        diplay[((vx + bit_pos) % WIDTH) + ((vy + index) % HEIGHT) * WIDTH] ^= bit;
+                        if diplay[((vx + bit_pos) % WIDTH) + ((vy + index) % HEIGHT) * WIDTH] == 0
+                            && bit == 1
+                        {
+                            state.v[0xf] = 1;
+                        }
+                    }
+                }
+            }
             _ if (opcode & 0xF0FF) == 0xE09E => {
                 info!(
                     "{:#06x} Skip next instruction if key with the value of Vx is pressed",
@@ -218,34 +361,7 @@ fn start_cpu(state: &mut CpuState) {
                 return;
             }
         };
-        count += 1;
         state.pc += 2;
-    }
-}
-
-struct CpuState {
-    memory: [u8; 4096],
-    v: [u8; 16],
-    i: u16,
-    dt: u8,
-    st: u16,
-    pc: u16,
-    sp: u8,
-    rng: SmallRng,
-}
-
-impl CpuState {
-    fn new() -> CpuState {
-        return CpuState {
-            memory: [0; 4096],
-            v: [0; 16],
-            i: 0,
-            dt: 0,
-            st: 0,
-            pc: 0x200,
-            sp: 0,
-            rng: SmallRng::seed_from_u64(12345),
-        };
     }
 }
 
@@ -254,11 +370,12 @@ trait OpCode {
     fn get_xy(&self) -> (usize, usize);
     fn get_xkk(&self) -> (usize, u8);
 }
+
 impl OpCode for u16 {
     #[inline]
     fn get_xy(&self) -> (usize, usize) {
-        let x = ((self & 0x0F00) >> 8) as usize;
-        let y = ((self & 0x00F0) >> 4) as usize;
+        let x: usize = ((self & 0x0F00) >> 8) as usize;
+        let y: usize = ((self & 0x00F0) >> 4) as usize;
         (x, y)
     }
 
