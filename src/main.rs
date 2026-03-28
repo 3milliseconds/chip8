e20ab454e4a32b43931bf302df8efcc21b5c16b9
3ms <258579313+3milliseconds@users.noreply.github.com>
2026-03-28 11:07:44 +0530

    clean up logs and convert range to bitwise &

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use log::error;
 use log::info;
 use rand::{RngExt, SeedableRng, rngs::SmallRng};
 fn main() {
@@ -10,160 +11,177 @@ fn main() {
 
 fn start_cpu(state: &mut CpuState) {
     let mut count = 0;
-    while count < 10 {
+    while count < 50 {
         let pc = state.pc as usize;
         let opcode: u16 = ((state.memory[pc] as u16) << 8) | (state.memory[pc + 1] as u16);
-        info!("Instruction: {:#x}", opcode);
         match opcode {
             0x00E0 => {
-                info!("Clear the display.")
+                info!("{:#06x} Clear the display", opcode);
             }
             0x00EE => {
-                info!("return from sub routine")
+                info!("{:#06x} Return from sub routine", opcode);
+                todo!("not implemented");
             }
-            0x1000..=0x1FFF => {
-                info!("sets the program counter to nnn.");
+            _ if (opcode & 0xF000) == 0x1000 => {
+                let nnn = opcode & 0x0FFF;
+                info!("{:#06x} Sets the program counter to {nnn}", opcode);
                 state.pc = opcode & 0x0FFF;
+                continue;
             }
-            0x2000..=0x2FFF => {
-                info!("Call subroutine at nnn.")
+            _ if (opcode & 0xF000) == 0x2000 => {
+                info!("Call subroutine at nnn");
+                todo!("not implemented");
             }
-            0x3000..=0x3FFF => {
-                info!("Skip next instruction if Vx = kk.");
+            _ if (opcode & 0xF000) == 0x3000 => {
                 let (x, kk) = opcode.get_xkk();
+                info!("{:#06x} Skip next instruction if V[{x}] = {kk}", opcode);
                 if state.v[x] == kk {
                     state.pc += 2;
                 }
             }
-            0x4000..=0x4FFF => {
-                info!("Skip next instruction if Vx != kk.");
+            _ if (opcode & 0xF000) == 0x4000 => {
                 let (x, kk) = opcode.get_xkk();
+                info!("{:#06x} Skip next instruction if V[{x}] != {kk}", opcode);
                 if state.v[x] == kk {
                     state.pc += 2;
                 }
             }
-            0x5000..=0x5FFF => {
-                info!("Skip next instruction if Vx = Vy.");
+            _ if (opcode & 0xF000) == 0x5000 => {
                 let (x, y) = opcode.get_xy();
+                info!("{:#06x} Skip next instruction if V[{x}] = V[{y}]", opcode);
                 if state.v[x] == state.v[y] {
                     state.pc += 2;
                 }
             }
-            0x6000..=0x6FFF => {
-                info!("Set Vx = kk.");
+            _ if (opcode & 0xF000) == 0x6000 => {
                 let (x, kk) = opcode.get_xkk();
+                info!("{:#06x} Set V[{x}] = {kk}", opcode);
                 state.v[x] = kk;
             }
-            0x7000..=0x7FFF => {
-                info!("Set Vx = Vx + kk.");
+            _ if (opcode & 0xF000) == 0x7000 => {
                 let (x, kk) = opcode.get_xkk();
+                info!("{:#06x} Set V[{x}] = V{x} + {kk}", opcode);
                 state.v[x as usize] += kk;
             }
             _ if (opcode & 0xF00F) == 0x8000 => {
-                info!("Set Vx = Vy.");
+                info!("{:#06x} Set Vx = Vy", opcode);
                 let (x, y) = opcode.get_xy();
                 state.v[x as usize] = state.v[y as usize];
             }
             _ if (opcode & 0xF00F) == 0x8001 => {
-                info!("Set Vx = Vx OR Vy.");
+                info!("{:#06x} Set Vx = Vx OR Vy", opcode);
                 let (x, y) = opcode.get_xy();
                 state.v[x as usize] = state.v[x as usize] ^ state.v[y as usize];
             }
             _ if (opcode & 0xF00F) == 0x8003 => {
-                info!("Set Vx = Vx AND Vy.");
+                info!("{:#06x} Set Vx = Vx AND Vy", opcode);
                 let (x, y) = opcode.get_xy();
                 state.v[x as usize] = state.v[x as usize] & state.v[y as usize];
             }
             _ if (opcode & 0xF00F) == 0x8004 => {
-                info!("Set Vx = Vx + Vy, set VF = carry.");
+                info!("{:#06x} Set Vx = Vx + Vy, set VF = carry", opcode);
                 let (x, y) = opcode.get_xy();
                 let carry;
                 (state.v[x], carry) = state.v[x].overflowing_add(state.v[y]);
                 state.v[0xf] = if carry { 1 } else { 0 }
             }
             _ if (opcode & 0xF00F) == 0x8005 => {
-                info!("Set Vx = Vx - Vy, set VF = NOT borrow.");
+                info!("{:#06x} Set Vx = Vx - Vy, set VF = NOT borrow", opcode);
                 let (x, y) = opcode.get_xy();
                 let overflow: bool;
                 (state.v[x], overflow) = state.v[x].overflowing_sub(state.v[y]);
                 state.v[0xf] = if !overflow { 1 } else { 0 };
             }
             _ if (opcode & 0xF00F) == 0x8006 => {
-                info!("Set Vx = Vx SHR 1.");
+                info!("{:#06x} Set Vx = Vx SHR 1", opcode);
                 let (x, _) = opcode.get_xy();
                 state.v[0xf] = if state.v[x] & 0x1 == 1 { 1 } else { 0 };
                 state.v[x] = state.v[x] >> 1;
             }
             _ if (opcode & 0xF00F) == 0x8007 => {
-                info!("Set Vx = Vy - Vx, set VF = NOT borrow.");
+                info!("{:#06x} Set Vx = Vy - Vx, set VF = NOT borrow", opcode);
                 let (x, y) = opcode.get_xy();
                 let overflow: bool;
                 (state.v[x], overflow) = state.v[y].overflowing_sub(state.v[x]);
                 state.v[0xf] = if !overflow { 1 } else { 0 };
             }
             _ if (opcode & 0xF00F) == 0x800E => {
-                info!("Set Vx = Vx SHL 1.");
+                info!("{:#06x} Set Vx = Vx SHL 1", opcode);
                 let (x, _) = opcode.get_xy();
                 state.v[0xf] = if (state.v[x] & 0x80) == 0x80 { 1 } else { 0 };
                 state.v[x] = state.v[x] << 1;
             }
-            _ if (opcode & 0x9000) == 0x9000 => {
-                info!("Skip next instruction if Vx != Vy.");
+            _ if (opcode & 0xF000) == 0x9000 => {
                 let (x, y) = opcode.get_xy();
+                info!("{:#06x} Skip next instruction if V[{x}] != V[{y}]", opcode);
                 if state.v[x] != state.v[y] {
                     state.pc += 2;
                 }
             }
-            0xA000..=0xAFFF => {
-                info!("Set I = nnn.");
-                state.i = opcode.get_nnn();
+            _ if (opcode & 0xF000) == 0xA000 => {
+                let nnn = opcode.get_nnn();
+                info!("{:#06x} Set I = {nnn}", opcode);
+                state.i = nnn;
             }
-            0xB000..=0xBFFF => {
-                info!("Jump to location nnn + V0.");
+            _ if (opcode & 0xF000) == 0xB000 => {
+                info!("{:#06x} Jump to location nnn + V0", opcode);
                 state.pc = opcode.get_nnn() + (state.v[0] as u16);
             }
-            0xC000..=0xCFFF => {
-                info!("Set Vx = random byte AND kk.");
+            _ if (opcode & 0xF000) == 0xC000 => {
+                info!("{:#06x} Set Vx = random byte AND kk", opcode);
                 let (x, kk) = opcode.get_xkk();
                 state.v[x] = state.rng.random::<u8>() & kk;
             }
-            0xD000..=0xDFFF => info!(
-                "Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision."
+            _ if (opcode & 0xF000) == 0xD000 => info!(
+                "{:#06x} Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision",
+                opcode
             ),
             _ if (opcode & 0xF0FF) == 0xE09E => {
-                info!("Skip next instruction if key with the value of Vx is pressed.");
+                info!(
+                    "{:#06x} Skip next instruction if key with the value of Vx is pressed",
+                    opcode
+                );
             }
             _ if (opcode & 0xF0FF) == 0xE0A1 => {
-                info!("Skip next instruction if key with the value of Vx is not pressed.")
+                info!(
+                    "{:#06x} Skip next instruction if key with the value of Vx is not pressed",
+                    opcode
+                )
             }
             _ if (opcode & 0xF0FF) == 0xF007 => {
-                info!("Set Vx = delay timer value.");
+                info!("{:#06x} Set Vx = delay timer value", opcode);
                 let (x, _) = opcode.get_xy();
                 state.v[x] = state.dt as u8;
             }
             _ if (opcode & 0xF0FF) == 0xF00A => {
-                info!("Wait for a key press, store the value of the key in Vx.")
+                info!(
+                    "{:#06x} Wait for a key press, store the value of the key in Vx",
+                    opcode
+                )
             }
-            _ if (opcode * 0xF0FF) == 0xF015 => {
-                info!("Set delay timer = Vx.");
+            _ if (opcode & 0xF0FF) == 0xF015 => {
+                info!("{:#06x} Set delay timer = Vx", opcode);
                 let (x, _) = opcode.get_xy();
                 state.dt = state.v[x]
             }
-            _ if (opcode * 0xF0FF) == 0xF018 => {
-                info!("Set sound timer = Vx.");
+            _ if (opcode & 0xF0FF) == 0xF018 => {
+                info!("{:#06x} Set sound timer = Vx", opcode);
                 let (x, _) = opcode.get_xy();
                 state.dt = state.v[x];
             }
-            _ if (opcode * 0xF0FF) == 0xF01E => {
-                info!("Set I = I + Vx.");
+            _ if (opcode & 0xF0FF) == 0xF01E => {
+                info!("{:#06x} Set I = I + Vx", opcode);
                 let (x, _) = opcode.get_xy();
                 state.i = state.i + (state.v[x] as u16);
             }
-            _ if (opcode * 0xF0FF) == 0xF029 => {
-                info!("Set I = location of sprite for digit Vx.")
+            _ if (opcode & 0xF0FF) == 0xF029 => {
+                info!("{:#06x} Set I = location of sprite for digit Vx", opcode)
             }
-            _ if (opcode * 0xF0FF) == 0xF033 => {
-                info!("Store BCD representation of Vx in memory locations I, I+1, and I+2.");
+            _ if (opcode & 0xF0FF) == 0xF033 => {
+                info!(
+                    "{:#06x} Store BCD representation of Vx in memory locations I, I+1, and I+2",
+                    opcode
+                );
                 let (x, _) = opcode.get_xy();
                 let mut r = state.v[x];
                 let h = r / 100;
@@ -176,20 +194,29 @@ fn start_cpu(state: &mut CpuState) {
                 state.memory[pos + 1] = t;
                 state.memory[pos + 2] = o;
             }
-            _ if (opcode * 0xF0FF) == 0xF055 => {
-                info!("Store registers V0 through Vx in memory starting at location I.");
+            _ if (opcode & 0xF0FF) == 0xF055 => {
+                info!(
+                    "{:#06x} Store registers V0 through Vx in memory starting at location I",
+                    opcode
+                );
                 let offset = state.i as usize;
                 state.memory[offset..offset + 0x10].copy_from_slice(&state.v);
             }
-            _ if (opcode * 0xF0FF) == 0xF065 => {
-                info!("Read registers V0 through Vx from memory starting at location I.");
+            _ if (opcode & 0xF0FF) == 0xF065 => {
+                info!(
+                    "{:#06x} Read registers V0 through Vx from memory starting at location I",
+                    opcode
+                );
                 let offset = state.i as usize;
                 state
                     .v
                     .copy_from_slice(&state.memory[offset..offset + 0x10]);
             }
 
-            _ => info!("got something else: {:?}", opcode),
+            _ => {
+                error!("{:#06x} Unknown instruction", opcode);
+                return;
+            }
         };
         count += 1;
         state.pc += 2;
